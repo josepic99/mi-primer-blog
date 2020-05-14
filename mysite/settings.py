843d52747f2a3f9ae2ba925032f7d116b78de467
jose <jose.picos@uabc.edu.mx>
2020-05-14 13:24:25 -0700

    Ley

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'crispy_forms',
-    'blog.apps.BlogConfig',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "crispy_forms",
+    "blog.apps.BlogConfig",
 ]
 
 MIDDLEWARE = [
